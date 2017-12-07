9c2a884b3dfb05bf4bc9002009aec5df4c5dd4b4
aarontreinish <32073791+aarontreinish@users.noreply.github.com>
2017-12-07 14:25:14 -0500

    Fixed boss damage bug
    When going to the top of the screen against the boss you do not take damage anymore.

--- src/mainGame/Player.java
+++ src/mainGame/Player.java
@@ -26,13 +26,23 @@ public class Player extends GameObject {
 	public static int playerSpeed = 10;
 	private Color playerColor = Color.white;
 
-
+	
+/**
+ * 
+ * @param x
+ * @param y
+ * @param id
+ * @param handler
+ * @param hud
+ * @param game
+ */
 	public Player(double x, double y, ID id, Handler handler, HUD hud, Game game) {
 		super(x, y, id);
 		this.handler = handler;
 		this.hud = hud;
 		this.game = game;
 		this.damage = 2;
+		
 		playerWidth = 32;
 		playerHeight = 32;
 	}
@@ -49,14 +59,19 @@ public class Player extends GameObject {
 
 		collision();
 		checkIfDead();
+		
+	
 
 	}
 
 	public void checkIfDead() {
+		
 		if (hud.health <= 0) {// player is dead, game over!
+			
 
 			if (hud.getExtraLives() == 0) {
 				game.gameState = STATE.GameOver;
+				hud.CheckScore();
 			}
 
 			else if (hud.getExtraLives() > 0) {// has an extra life, game continues
@@ -64,16 +79,16 @@ public class Player extends GameObject {
 				hud.setHealth(100);
 			}
 		}
+		
 	}
 
 	/**
 	 * Checks for collisions with all of the enemies, and handles it accordingly
 	 */
 	public void collision() {
-
+		
 		hud.updateScoreColor(Color.white);
 		updatepPlayerColor(Color.white);
-
 		for (int i = 0; i < handler.object.size(); i++) {
 			GameObject tempObject = handler.object.get(i);
 
@@ -88,22 +103,25 @@ public class Player extends GameObject {
 					hud.health -= damage;
 					hud.updateScoreColor(Color.red);
 					updatepPlayerColor(Color.red);
-
+					
+					
 				}
 
 			}
-			if (tempObject.getId() == ID.EnemyBoss) {
+			//if (tempObject.getId() == ID.EnemyBoss) {
 				// Allows player time to get out of upper area where they will get hurt once the
 				// boss starts moving
-				if (this.y <= 138 && tempObject.isMoving) {
-					hud.health -= 2;
-					hud.updateScoreColor(Color.red);
-					updatepPlayerColor(Color.red);
+				//if (this.y <= 138 && tempObject.isMoving) {
+					//hud.health -= 2;
+					//hud.updateScoreColor(Color.red);
+					//updatepPlayerColor(Color.red);
 
+					
 				}
-			}
+			
 
-		}
+		
+		
 	}
 
 	@Override
@@ -111,6 +129,7 @@ public class Player extends GameObject {
 
 		g.setColor(playerColor);
 		g.fillRect((int) x, (int) y, playerWidth, playerHeight);
+		
 
 	}
 
@@ -131,5 +150,6 @@ public class Player extends GameObject {
 	public void updatepPlayerColor(Color color) {
 		this.playerColor = color;
 	}
-
+	
+	
 }
